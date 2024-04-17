16059c3bcd4e41866902bba54888f7a4ab4d6260
yuya-3110 <ysuu45143745@gmail.com>
2024-04-18 00:56:55 +0900

    first commit

---
+++ YuchinanteKadai15/ItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemTableViewCell.swift
+//  YuchinanteKadai15
+//  
+//  Created by Yuchinante on 2024/04/16
+//  
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
